4ccc8bd3c1235fd2fd8d700d0ae942d705fba8ca
Jack Andersen <jackoalan@gmail.com>
2016-07-31 10:27:28 -1000

    Use constexpr for evaluating bitwise enums

--- include/kabufuda/Util.hpp
+++ include/kabufuda/Util.hpp
@@ -36,29 +36,29 @@
 
 #ifndef ENABLE_BITWISE_ENUM
 #define ENABLE_BITWISE_ENUM(type)\
-inline type operator|(type a, type b)\
+constexpr type operator|(type a, type b)\
 {\
     using T = std::underlying_type_t<type>;\
     return type(static_cast<T>(a) | static_cast<T>(b));\
 }\
-inline type operator&(type a, type b)\
+constexpr type operator&(type a, type b)\
 {\
     using T = std::underlying_type_t<type>;\
     return type(static_cast<T>(a) & static_cast<T>(b));\
 }\
-inline type& operator|=(type& a, const type& b)\
+constexpr type& operator|=(type& a, const type& b)\
 {\
     using T = std::underlying_type_t<type>;\
     a = type(static_cast<T>(a) | static_cast<T>(b));\
     return a;\
 }\
-inline type& operator&=(type& a, const type& b)\
+constexpr type& operator&=(type& a, const type& b)\
 {\
     using T = std::underlying_type_t<type>;\
     a = type(static_cast<T>(a) & static_cast<T>(b));\
     return a;\
 }\
-inline type operator~(const type& key)\
+constexpr type operator~(const type& key)\
 {\
     using T = std::underlying_type_t<type>;\
     return type(~static_cast<T>(key));\
