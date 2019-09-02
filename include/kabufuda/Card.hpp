7c01ada2e673f16cfd56ebe54202ac5d33d202cd
Lioncash <mathew1800@gmail.com>
2019-09-02 08:30:06 -0400

    General: Use explicit where applicable
    Makes conversions explicit in order to prevent error-prone implicit
    conversions.

--- include/kabufuda/Card.hpp
+++ include/kabufuda/Card.hpp
@@ -24,7 +24,7 @@ class FileHandle {
 public:
   FileHandle() = default;
   uint32_t getFileNo() const { return idx; }
-  operator bool() const { return getFileNo() != UINT32_MAX; }
+  explicit operator bool() const { return getFileNo() != UINT32_MAX; }
 };
 
 struct ProbeResults {
@@ -136,7 +136,7 @@ public:
    * @param game  The game code.
    * @param maker The maker code.
    */
-  Card(const char* game = nullptr, const char* maker = nullptr);
+  explicit Card(const char* game = nullptr, const char* maker = nullptr);
   ~Card();
 
   /**
@@ -489,6 +489,6 @@ public:
   /**
    * @return Whether or not the card is within a ready state.
    */
-  operator bool() const { return getError() == ECardResult::READY; }
+  explicit operator bool() const { return getError() == ECardResult::READY; }
 };
 } // namespace kabufuda
