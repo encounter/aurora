7fecb6aaf530af8dba75e755530d0379a612adf0
CreateSource <72283721+abnormalhare@users.noreply.github.com>
2025-04-23 00:17:14 -0400

    Update gx_shader.cpp

--- lib/gfx/gx_shader.cpp
+++ lib/gfx/gx_shader.cpp
@@ -863,7 +863,7 @@ wgpu::ShaderModule build_shader(const ShaderConfig& config, const ShaderInfo& in
           var dist_attn = dot(light.dist_att, vec3f(1.0, dist, dist2));
           attn = max(0.0, cos_attn / dist_attn);)""");
       } else if (cc.attnFn == GX_AF_SPEC) {
-        std::string normal = (UsePerPixelLighting) ? "in.mv_nrm" : "mv_nrm";
+        std::string normal = UsePerPixelLighting ? "in.mv_nrm" : "mv_nrm";
         lightAttnFn = fmt::format(R"""(
           var cosine = select(0.0, max(0.0, dot({0}, light.dir)), dot({0}, ldir) >= 0.0);
           var cos_attn = dot(light.cos_att, vec3f(1.0, attn, attn * attn));
