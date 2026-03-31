3f722084f9a7a886b07a091c935c7ddb0ad6956f
Irastris <irastris15@gmail.com>
2026-03-31 00:23:41 -0400

    Use INT8_MAX instead of a magic 127

--- lib/dolphin/pad/pad.cpp
+++ lib/dolphin/pad/pad.cpp
@@ -375,8 +375,8 @@ void ClampStick(int8_t* px, int8_t* py, int8_t max, int8_t xy, int8_t min) {
     return;
   }
 
-  x = (x * max) / (127 - min);
-  y = (y * max) / (127 - min);
+  x = (x * max) / (INT8_MAX - min);
+  y = (y * max) / (INT8_MAX - min);
 
   if (xy * y <= xy * x) {
     int32_t d = xy * x + (max - xy) * y;
