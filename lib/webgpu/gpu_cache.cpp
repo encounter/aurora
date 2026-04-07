f7439e20e0e30109f187453bf2a18667f9c04778
PJB3005 <pieterjan.briers+git@gmail.com>
2026-04-07 19:45:31 +0200

    Stop using exceptions for error handling

--- lib/webgpu/gpu_cache.cpp
+++ lib/webgpu/gpu_cache.cpp
@@ -26,27 +26,6 @@ static void init_abort() {
   db = nullptr;
 }
 
-class SqliteError : public std::runtime_error {
-public:
-  explicit SqliteError(const std::string& str) : runtime_error(str) {
-  }
-
-  explicit SqliteError(const char* what) : runtime_error(what) {
-  }
-
-  static SqliteError ConsumeMsg(char* errmsg) {
-    auto result = SqliteError(errmsg);
-    sqlite3_free(errmsg);
-    return result;
-  }
-};
-
-static void sqlite_check(int ret) {
-  if (ret != SQLITE_OK) {
-    throw SqliteError(sqlite3_errmsg(db));
-  }
-}
-
 static int sqlite3_exec(const char* sql) {
   return sqlite3_exec(db, sql, nullptr, nullptr, nullptr);
 }
@@ -69,24 +48,50 @@ static int sqlite3_exec(const char* sql, T callback, char** errmsg) {
 }
 
 struct SqliteTransaction {
-  bool committed;
-  explicit SqliteTransaction(bool immediate = false) : committed(false) {
-    auto type = immediate ? "BEGIN IMMEDIATE" : "BEGIN";
-    sqlite_check(sqlite3_exec(db, type, nullptr, nullptr, nullptr));
+  bool need_rollback;
+  explicit SqliteTransaction(bool immediate = false) : need_rollback(true) {
+    const auto type = immediate ? "BEGIN IMMEDIATE" : "BEGIN";
+    const auto ret = sqlite3_exec(db, type, nullptr, nullptr, nullptr);
+    if (ret != SQLITE_OK) {
+      Log.error("Failed to start transaction: {}", sqlite3_errmsg(db));
+      need_rollback = false;
+    }
   }
   ~SqliteTransaction() {
-    if (!committed) {
-      sqlite_check(sqlite3_exec(db, "ROLLBACK", nullptr, nullptr, nullptr));
+    if (need_rollback) {
+      const auto ret = sqlite3_exec(db, "ROLLBACK", nullptr, nullptr, nullptr);
+      if (ret != SQLITE_OK) {
+        Log.error("Failed to roll back transaction (uh oh?): {}", sqlite3_errmsg(db));
+      }
     }
   }
   void commit() {
-    sqlite_check(sqlite3_exec(db, "COMMIT", nullptr, nullptr, nullptr));
-    committed = true;
+    const auto ret = sqlite3_exec(db, "COMMIT", nullptr, nullptr, nullptr);
+    if (ret != SQLITE_OK) {
+      Log.error("Failed to commit transaction: {}", sqlite3_errmsg(db));
+      return;
+    }
+    need_rollback = false;
+  }
+  explicit operator bool() const {
+    return need_rollback;
   }
 };
 
-static void ensure_schema_up_to_date() {
+static int check(int ret) {
+  if (ret != SQLITE_OK) {
+    Log.error("SQLite operation failed: {}", sqlite3_errmsg(db));
+  }
+
+  return ret;
+}
+
+static bool ensure_schema_up_to_date() {
   SqliteTransaction tx(true);
+  if (!tx) {
+    Log.error("Failed to open schema check transaction", sqlite3_errmsg(db));
+    return false;
+  }
 
   auto ret = sqlite3_exec(
     db,
@@ -94,7 +99,10 @@ static void ensure_schema_up_to_date() {
     nullptr,
     nullptr,
     nullptr);
-  sqlite_check(ret);
+  if (ret != SQLITE_OK) {
+    Log.error("Failed to create schema table: {}", sqlite3_errmsg(db));
+    return false;
+  }
 
   bool match = false;
   auto cmd = fmt::format("SELECT * FROM aurora_schema WHERE value = {}", CACHE_SCHEMA);
@@ -102,10 +110,13 @@ static void ensure_schema_up_to_date() {
     cmd.c_str(),
     [&match](int, char**, char**) {match = true;},
     nullptr);
-  sqlite_check(ret);
+  if (ret != SQLITE_OK) {
+    Log.error("Failed to check schema table: {}", sqlite3_errmsg(db));
+    return false;
+  }
 
   if (match) {
-    return;
+    return true;
   }
 
   cmd = fmt::format(
@@ -114,9 +125,51 @@ static void ensure_schema_up_to_date() {
     "DELETE FROM aurora_schema;"
     "INSERT INTO aurora_schema VALUES ({});", CACHE_SCHEMA);
   ret = sqlite3_exec(db, cmd.c_str(), nullptr, nullptr, nullptr);
-  sqlite_check(ret);
+  if (ret != SQLITE_OK) {
+    Log.error("Failed to update schema: {}", sqlite3_errmsg(db));
+    return false;
+  }
 
   tx.commit();
+  return true;
+}
+
+static bool cache_init_core() {
+  Log.debug("SQLite version {}", sqlite3_libversion());
+
+  std::string file = fmt::format("{}{}", g_config.configPath, "dawn_cache.db");
+  Log.debug("Using dawn cache at {}", file);
+  auto ret = sqlite3_open(file.c_str(), &db);
+  if (ret != SQLITE_OK) {
+    Log.error("Failed to open database: {}", sqlite3_errmsg(db));
+    return false;
+  }
+
+  // WAL mode + NORMAL = no need for disk syncs, consistent but not durable is fine.
+  ret = sqlite3_exec("PRAGMA journal_mode=WAL; PRAGMA synchronous=NORMAL;");
+  if (ret != SQLITE_OK) {
+    Log.error("Failed to set pragmas: {}", sqlite3_errmsg(db));
+    return false;
+  }
+
+  if (!ensure_schema_up_to_date()) {
+    Log.error("Failed to validate schema");
+    return false;
+  }
+
+  ret = sqlite3_prepare_v3(db, "SELECT value, size FROM cache WHERE key = ?", -1, SQLITE_PREPARE_PERSISTENT, &load_stmt, nullptr);
+  if (ret != SQLITE_OK) {
+    Log.error("Failed to prepare statement: {}", sqlite3_errmsg(db));
+    return false;
+  }
+
+  ret = sqlite3_prepare_v3(db, "REPLACE INTO cache (key, value, size) VALUES (?, ?, ?)", -1, SQLITE_PREPARE_PERSISTENT, &store_stmt, nullptr);
+  if (ret != SQLITE_OK) {
+    Log.error("Failed to prepare statement: {}", sqlite3_errmsg(db));
+    return false;
+  }
+
+  return true;
 }
 
 static bool cache_init() {
@@ -128,23 +181,8 @@ static bool cache_init() {
     return true;
   }
 
-  try {
-    Log.debug("SQLite version {}", sqlite3_libversion());
-
-    std::string file = fmt::format("{}{}", g_config.configPath, "dawn_cache.db");
-    Log.debug("Using dawn cache at {}", file);
-    auto ret = sqlite3_open(file.c_str(), &db);
-    sqlite_check(ret);
-
-    // WAL mode + NORMAL = no need for disk syncs, consistent but not durable is fine.
-    sqlite_check(sqlite3_exec("PRAGMA journal_mode=WAL; PRAGMA synchronous=NORMAL;"));
-
-    ensure_schema_up_to_date();
-
-    sqlite_check(sqlite3_prepare_v3(db, "SELECT value, size FROM cache WHERE key = ?", -1, SQLITE_PREPARE_PERSISTENT, &load_stmt, nullptr));
-    sqlite_check(sqlite3_prepare_v3(db, "REPLACE INTO cache (key, value, size) VALUES (?, ?, ?)", -1, SQLITE_PREPARE_PERSISTENT, &store_stmt, nullptr));
-  } catch (SqliteError& e) {
-    Log.error("SQLite DB init failed: {}", e.what());
+  if (!cache_init_core()) {
+    Log.error("SQLite DB init failed");
     init_abort();
     return false;
   }
@@ -161,45 +199,42 @@ size_t load_from_cache(void const* key, size_t keySize, void* value, size_t valu
     return 0;
   }
 
-  try {
-    SqliteTransaction tx;
-
-    const auto keyHash = XXH128(key, keySize, 0);
-    sqlite_check(sqlite3_bind_blob(load_stmt, 1, &keyHash, sizeof(keyHash), SQLITE_TRANSIENT));
-
-    const auto ret = sqlite3_step(load_stmt);
-    size_t foundSize;
-    if (ret == SQLITE_ROW) {
-      // Hit
-      const auto foundPtr = sqlite3_column_blob(load_stmt, 0);
-      foundSize = sqlite3_column_int64(load_stmt, 1);
-
-      if (value && valueSize == foundSize) {
-        const auto compSize = sqlite3_column_bytes(load_stmt, 0);
-
-        const auto zstdRet = ZSTD_decompress(value, valueSize, foundPtr, compSize);
-        if (ZSTD_isError(zstdRet)) {
-          Log.error("zstd decompression error: {}", ZSTD_getErrorName(zstdRet));
-          foundSize = 0;
-        }
-      }
-
-    } else if (ret == SQLITE_DONE) {
-      // Miss
-      foundSize = 0;
-    } else {
-      // Error
-      sqlite_check(ret);
-      abort(); // Should be unreachable, but let's make sure.
-    }
-
-    sqlite3_reset(load_stmt);
-
-    return foundSize;
-  } catch (SqliteError& e) {
-    Log.error("Read from cache failed: {}", e.what());
+  SqliteTransaction tx;
+  if (!tx) {
+    Log.error("Failed to open load transaction");
     return 0;
   }
+
+  const auto keyHash = XXH128(key, keySize, 0);
+  check(sqlite3_bind_blob(load_stmt, 1, &keyHash, sizeof(keyHash), SQLITE_TRANSIENT));
+
+  const auto ret = sqlite3_step(load_stmt);
+  size_t foundSize;
+  if (ret == SQLITE_ROW) {
+    // Hit
+    const auto foundPtr = sqlite3_column_blob(load_stmt, 0);
+    foundSize = sqlite3_column_int64(load_stmt, 1);
+
+    if (value && valueSize == foundSize) {
+      const auto compSize = sqlite3_column_bytes(load_stmt, 0);
+
+      const auto zstdRet = ZSTD_decompress(value, valueSize, foundPtr, compSize);
+      if (ZSTD_isError(zstdRet)) {
+        Log.error("zstd decompression error: {}", ZSTD_getErrorName(zstdRet));
+        foundSize = 0;
+      }
+    }
+  } else if (ret == SQLITE_DONE) {
+    // Miss
+    foundSize = 0;
+  } else {
+    Log.error("Looking up cache key failed: {}", sqlite3_errmsg(db));
+    return 0;
+  }
+
+  check(sqlite3_reset(load_stmt));
+
+  return foundSize;
 }
 
 void store_to_cache(void const* key, size_t keySize, void const* value, size_t valueSize, void*) {
@@ -209,51 +244,50 @@ void store_to_cache(void const* key, size_t keySize, void const* value, size_t v
     return;
   }
 
-  try {
-    SqliteTransaction tx(true);
-
-    const auto bound = ZSTD_compressBound(valueSize);
-    if (ZSTD_isError(bound)) {
-      Log.error("Failed to calculate ZSTD_compressBound: {}", ZSTD_getErrorName(bound));
-      return;
-    }
-
-    if (compress_buffer.size() < bound)
-      compress_buffer.resize(bound);
-
-    auto compressRet = ZSTD_compress(compress_buffer.data(), compress_buffer.size(), value, valueSize, 0);
-    if (ZSTD_isError(compressRet)) {
-      Log.error("ZSTD compression error: {}", ZSTD_getErrorName(compressRet));
-      return;
-    }
-
-    const auto keyHash = XXH128(key, keySize, 0);
-    sqlite_check(sqlite3_bind_blob64(store_stmt, 1, &keyHash, sizeof(keyHash), SQLITE_TRANSIENT));
-    sqlite_check(sqlite3_bind_blob64(store_stmt, 2, compress_buffer.data(), compressRet, SQLITE_STATIC));
-    sqlite_check(sqlite3_bind_int64(store_stmt, 3, static_cast<sqlite3_int64>(valueSize)));
-
-    const auto ret = sqlite3_step(store_stmt);
-    if (ret != SQLITE_DONE) {
-      // Error
-      sqlite_check(ret);
-      abort(); // Should be unreachable, but let's make sure.
-    }
-
-    sqlite_check(sqlite3_reset(store_stmt));
-    sqlite_check(sqlite3_bind_null(store_stmt, 2));
-
-    tx.commit();
-
-  } catch (SqliteError& e) {
-    Log.error("Store to cache failed: {}", e.what());
+  SqliteTransaction tx(true);
+  if (!tx) {
+    Log.error("Failed to open store transaction");
+    return;
   }
+
+  const auto bound = ZSTD_compressBound(valueSize);
+  if (ZSTD_isError(bound)) {
+    Log.error("Failed to calculate ZSTD_compressBound: {}", ZSTD_getErrorName(bound));
+    return;
+  }
+
+  if (compress_buffer.size() < bound)
+    compress_buffer.resize(bound);
+
+  auto compressRet = ZSTD_compress(compress_buffer.data(), compress_buffer.size(), value, valueSize, 0);
+  if (ZSTD_isError(compressRet)) {
+    Log.error("ZSTD compression error: {}", ZSTD_getErrorName(compressRet));
+    return;
+  }
+
+  const auto keyHash = XXH128(key, keySize, 0);
+  check(sqlite3_bind_blob64(store_stmt, 1, &keyHash, sizeof(keyHash), SQLITE_TRANSIENT));
+  check(sqlite3_bind_blob64(store_stmt, 2, compress_buffer.data(), compressRet, SQLITE_STATIC));
+  check(sqlite3_bind_int64(store_stmt, 3, static_cast<sqlite3_int64>(valueSize)));
+
+  const auto ret = sqlite3_step(store_stmt);
+  if (ret != SQLITE_DONE) {
+    // Error or something
+    Log.error("Failed to insert row: {}", sqlite3_errmsg(db));
+    return;
+  }
+
+  check(sqlite3_reset(store_stmt));
+  check(sqlite3_bind_null(store_stmt, 2));
+
+  tx.commit();
 }
 
 void cache_shutdown() {
   compress_buffer.clear();
-  sqlite3_finalize(load_stmt);
-  sqlite3_finalize(store_stmt);
-  sqlite3_close(db);
+  check(sqlite3_finalize(load_stmt));
+  check(sqlite3_finalize(store_stmt));
+  check(sqlite3_close(db));
   db = nullptr;
 }
 
