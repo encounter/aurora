e4ca85fc6a1013351159a5aa8fb1d6c47ce23d4d
Phillip Stephens <antidote.crk@gmail.com>
2026-04-16 09:35:43 -0700

    Add aurora::core library for aurora::ms

--- cmake/aurora_ms.cmake
+++ cmake/aurora_ms.cmake
@@ -3,3 +3,4 @@ add_library(aurora_ms STATIC lib/dolphin/ms/mouse.cpp)
 add_library(aurora::ms ALIAS aurora_ms)
 
 target_include_directories(aurora_ms PUBLIC include)
+target_link_libraries(aurora_ms PUBLIC aurora::core)
