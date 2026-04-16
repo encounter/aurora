3cc2281527e7cf35dc146d3565d916dfd25afb3e
Phillip Stephens <antidote.crk@gmail.com>
2026-04-16 09:38:03 -0700

    Add absl::flat_hash_map library for aurora::ms

--- cmake/aurora_ms.cmake
+++ cmake/aurora_ms.cmake
@@ -4,3 +4,4 @@ add_library(aurora::ms ALIAS aurora_ms)
 
 target_include_directories(aurora_ms PUBLIC include)
 target_link_libraries(aurora_ms PUBLIC aurora::core)
+target_link_libraries(aurora_ms PRIVATE absl::flat_hash_map)
